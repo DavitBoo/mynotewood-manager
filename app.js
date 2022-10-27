cfe96b6ed3f1ff5b9563bd372f47f24f74f6743f
DavitBoo <davidmarras@gmail.com>
2022-10-27 17:28:27 +0200

    session collection works

--- app.js
+++ app.js
@@ -7,12 +7,27 @@ require('dotenv').config()
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+const MongoStore = require('connect-mongo');
 
 
 //middleware
 app.use(express.static('./public'))
-//I do not need to install bodyparse anymore, with express.json(), it does the same in the latests versions
 app.use(express.json());
+
+console.log(MongoStore)
+
+const sessionStore = MongoStore.create({ mongoUrl: process.env.MONGO_URI, collection: 'sessions' })
+
+app.use(session({
+    secret: process.env.SECRET,
+    saveUninitialized: true,
+    resave: false,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+  }));
+
 
 //it will be running this middleware until we use the <<next()>>
 app.use('/api/v1/tasks', tasks)
@@ -23,6 +38,7 @@ const port = process.env.PORT || 3000
 
 const start =  async () =>{
     try {
+    // maybe I should use createConnection (?)
       await mongoose.connect(process.env.MONGO_URI)    //we are using process.env in order to access the .env file -it is a global variable-
         app.listen(port, console.log(`server is listening in port 3000 ....`));
     } catch (error) {
